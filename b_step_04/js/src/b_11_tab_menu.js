4ff5169f743da69f6c523818febd3dfe85d51a9f
Andrew1130 <yhyh273@gmail.com>
2022-02-08 23:54:04 +0900

    73 강
    실무예제 16-2 : 탭메뉴 구성을 js로 세팅

--- b_step_04/js/src/b_11_tab_menu.js
+++ b_step_04/js/src/b_11_tab_menu.js
@@ -9,6 +9,10 @@ ajax -> 비동기 방식
 
 (function($){
 
+//? ----------------------------------------
+//? STEP 1 : Json 데이터 불러오기
+//? ----------------------------------------
+
   //* $.getJSON으로도 json 파일 불러오기 가능
   // $.getJSON("../json/menu.json", function(data){
   //   console.log( data );
@@ -26,8 +30,8 @@ ajax -> 비동기 방식
     for(var i=0; i < len; i+=1 ){
       tabTitle.push( dataFile[i].type );
     }
-    
 
+    
   //? 탭메뉴 타이틀 설정 --------------------------------------- 
     //* 배열.filter(function(data, index){}); 
     /* 배열 중에서 원하는 조건에 맞는 데이터만 골라서(필터링해서) 가져오기 위해 사용 */
@@ -35,23 +39,123 @@ ajax -> 비동기 방식
 
 
     // 중복되는 배열의 값을 정리해주는 기능
-    var tabTitleCheck = tabTitle.filter(function(data, index){;
-      return tabTitle.indexOf(data) === index;
+    var tabTitleCheck = tabTitle.filter(function(data, index){; 
+      // console.log(data)
+      console.log(tabTitle.indexOf(data)) // 0, 16, 30, 42
+      return tabTitle.indexOf(data) === index; 
+      //* tabTitle 이라는 배열 중에서 index가 0, 16, 30, 42에 해당하는 데이터만 골라서(필터링해서) 반환
     });
+
+    console.log(tabTitleCheck)
+
 
 
   //? 탭메뉴 내용 구성에 맞는 목록 체크 --------------------------
     var tabMenuSet;
     var tabMenuFn = function(n) {  
       tabMenuSet = dataFile.filter(function(data){
+        // console.log(data)
         return data.type === tabTitleCheck[n];
+        //* dataFile(data) 이라는 배열 중에서, type이 tabTitleCheck[n]에 해당되는 데이터만 골라서(필터링해서) 반환 
       });
     }
-    tabMenuFn(0);
-    console.log(tabMenuSet);
+
+    // tabMenuFn(0)
+    // console.log(tabMenuSet) 
+    //* tabMenuFn의 n에 숫자가 대입되어야 내부의 filter 함수가 작동한다.
+
+    // tabMenuFn(2)
+    // console.log(tabMenuSet)
 
 
-  //* -----------------------------------------------------------------
+
+    //* title 구성 : tabTitleCheck
+    //* title에 따른 메뉴 구성 : 함수 호출 > tabMenuFn(순서);
+    //* title에 따른 메뉴 구성 : 구성사용 > tabMenuSet;
+
+
+
+//? --------------------------------------------------------
+//? STEP 2 : 불러온 Json data 기반으로 구성 배치
+//? --------------------------------------------------------
+
+
+
+  //TODO : 변수 (1차 기존 요소)
+  var tabArea = $('.tab_area');
+  var tabTitle = tabArea.find('.tab_title')
+  var tabContent = tabArea.find('.tab_content');
+
+  //* tabTitle 내부에 탭메뉴 제목부 구성 (tabTitleCheck 이용)
+  tabTitle.html('<ul></ul>');
+  var tabTUl = tabTitle.children('ul');
+  var titleEl = '<li><button type="button"></button></li>'
+  var tabTLen = tabTitleCheck.length;  
+
+  for( i=0 ; i<tabTLen ; i+=1  ){
+    tabTUl.append(titleEl);
+    tabTUl.find('button').eq(i).text(tabTitleCheck[i])
+  }
+
+  // $.each(tabTitleCheck, function(index, data){
+  //   tabTUl.append(titleEl);
+  //   tabTUl.find('button').eq(index).text(data)
+  // });
+
+  var tabTLi = tabTitle.find('li');
+  var tabBtn = tabTLi.find('button'); 
+  tabTLi.css({ width: (100 / tabTLen) + '%' });
+  tabTLi.eq(0).addClass('on');
+
+
+
+
+//? --------------------------------------------------------
+//? STEP 3 : 내용을 구성하기 위한 세팅 및 함수 처리
+//? --------------------------------------------------------
+
+
+
+  tabContent.html('<ul class="tab_list"></ul>')
+  var tabConMenu = tabContent.find('.tab_list');
+  var tabListSet = '<li><div class="img_con"><span></span></div><dl><dt></dt><dd class="text_con"></dd><dd class="link_con"><a href="#">자세히보기</a></dd></dl></li>';
+
+  
+  var tabMenuSetFn = function(p){
+    tabMenuFn(p);
+    tabConMenu.empty(); // ul 이하에 있던 기존 내용은 일단 모두 지운다.
+    // tabConMenu.text( JSON.stringify(tabMenuSet) );
+    
+
+    var l = 0;
+    var tabSetLen = tabMenuSet.length;
+    var tSet, hrefText, liIdx; // for문 내부에 var을 쓰는 것은 좋지 않으므로, for문 외부에 변수를 선언
+    
+    for(  ;  l < tabSetLen ; l += 1 ) {
+
+      tabConMenu.append(tabListSet);
+      tSet = tabMenuSet[l]
+      hrefText = './' + tSet.couse + './' + tSet.link;
+      liIdx = tabConMenu.children('li').eq(l);
+
+      liIdx.find('dt').text(tSet.menu);
+      liIdx.find('.text_con').text(tSet.account);
+      liIdx.find('a').attr({href:hrefText});
+
+      //background-image설정 빠져있음
+
+    } // for
+
+  }; // tabMenuSetFn();
+
+  tabMenuSetFn(0);
+
+
+
+//? --------------------------------------------------------
+//? STEP 4 : 탭메뉴 처리 설정
+//? --------------------------------------------------------
+
 
     //TODO : 구현할 기능
     /*
@@ -61,24 +165,22 @@ ajax -> 비동기 방식
 
 
     //TODO : 변수
-    var tabArea = $('.tab_area');
-    var tabTitle = tabArea.find('.tab_title')
     var tabTLi = tabTitle.find('li');
-    var tabBtn = tabTLi.find('button');
-    var tabContent = tabArea.find('.tab_content'); 
-    var tabConMenu = tabContent.find('.tab_list');
-
+    var tabBtn = tabTLi.find('button'); 
+    
 
     //TODO : 이벤트
     tabBtn.on('click', function(e){
       e.preventDefault();
-      var i = $(this).parent().index();
+      var k = $(this).parent().index();
 
-      tabBtn.parent().eq(i).addClass('on');
-      tabBtn.parent().eq(i).siblings().removeClass('on');
+      tabBtn.parent().eq(k).addClass('on');
+      tabBtn.parent().eq(k).siblings().removeClass('on');
 
-      tabConMenu.text( 'tab 구성' + (i+1) );
+      tabMenuSetFn(k); // 내비게이션 버튼을 클릭할 때 내비게이션 버튼의 index가  tabMenuSetFn 함수에 대입된다.
+
     });
+
 
 
   }); // $.ajax
