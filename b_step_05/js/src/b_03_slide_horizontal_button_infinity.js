f4db12eb90ee19258677440d869db6206b7e722b
Andrew1130 <yhyh273@gmail.com>
2022-02-10 11:52:26 +0900

    78 강
    실무예제 19 :  slide 버튼 클릭시 무한이동( 요소복제 후 처리)

--- b_step_05/js/src/b_03_slide_horizontal_button_infinity.js
+++ b_step_05/js/src/b_03_slide_horizontal_button_infinity.js
@@ -28,6 +28,34 @@ var permission = true;
 
 
 //? 함수 -------------
+var nextBtnFn = function(){
+  permission = false; // 출입 패스권 (승인취소)
+
+  horizonSlide.stop().animate({ marginLeft : -100 + "%" }, function(){
+  horizonDiv.eq(0).appendTo(horizonSlide); // 02-03-04-05-01
+  
+  horizonSlide.css({ marginLeft: 0 })
+  horizonDiv = horizonSlide.children('div');// 함수 외부에서는 horizonDiv의 순서가 appendTo로 변경되기 이전의 순서로 되어 있으므로, appendTo가 실행된 이후의 값으로 덮어 써 주어야 한다. (02-03-04-05-01)
+
+  permission = true;
+
+  });
+}
+
+var prevBtnFn = function(){
+  permission = false; 
+
+  horizonDiv.eq(-1).prependTo(horizonSlide); // div의 맨 마지막 순서를 맨 앞으로 붙인다.
+  horizonSlide.css({ marginLeft: -100+ '%' }); // 맨 마지막 요소가 맨 앞으로 와서, 맨 마지막 요소가 나타나는데, 애니메이션 적용을 위해 일단 한 칸 밀어 둔다.
+
+  horizonSlide.stop().animate({ marginLeft: 0 }, function(){
+  horizonDiv = horizonSlide.children('div');
+
+  permission = true;
+
+  }); // 애니메이션을 적용해 한 칸 밀면서 맨 마지막 요소를 불러오고, 이때의 div를 함수에 갱신한다.
+
+}
 
 
 //? 기능 수행 -----------
@@ -39,41 +67,16 @@ button.eq(0).on('click', function(e){
   e.preventDefault();
 
   if(permission) {
-
-    permission = false; // 출입 패스권 (승인취소)
-
-    horizonSlide.stop().animate({ marginLeft : -100 + "%" }, function(){
-    horizonDiv.eq(0).appendTo(horizonSlide); // 02-03-04-05-01
-    
-    horizonSlide.css({ marginLeft: 0 })
-    horizonDiv = horizonSlide.children('div');// 함수 외부에서는 horizonDiv의 순서가 appendTo로 변경되기 이전의 순서로 되어 있으므로, appendTo가 실행된 이후의 값으로 덮어 써 주어야 한다. (02-03-04-05-01)
-
-    permission = true;
-
-  });
-
+     nextBtnFn()
   }// if()
 })
-
 
 //* prev 버튼 클릭시 수행
 button.eq(1).on('click', function(e){
   e.preventDefault();
 
   if(permission) {
-
-    permission = false; 
-
-    horizonDiv.eq(-1).prependTo(horizonSlide); // div의 맨 마지막 순서를 맨 앞으로 붙인다.
-    horizonSlide.css({ marginLeft: -100+ '%' }); // 맨 마지막 요소가 맨 앞으로 와서, 맨 마지막 요소가 나타나는데, 애니메이션 적용을 위해 일단 한 칸 밀어 둔다.
-
-    horizonSlide.stop().animate({ marginLeft: 0 }, function(){
-    horizonDiv = horizonSlide.children('div');
-
-    permission = true;
-
-  }); // 애니메이션을 적용해 한 칸 밀면서 맨 마지막 요소를 불러오고, 이때의 div를 함수에 갱신한다.
-
+    prevBtnFn()
   }// if()
 })
 
