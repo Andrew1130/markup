8dad1c9e6b9e863c578a75db6cb8fdeb12a3fd8e
Andrew1130 <yhyh273@gmail.com>
2022-02-15 17:50:15 +0900

    85강
    슬라이드 수행

--- c_project.01/code/js/src/main_ad_slide.js
+++ c_project.01/code/js/src/main_ad_slide.js
@@ -3,7 +3,7 @@
 
 
 (function($){
-//? 구현할 기능 -----------
+
 /*
 - 외부 데이터 불러와서 확인
 >> $.ajax({ url:'jsondataUrl', dataType:'json', context:document.body }).done(function(data){})
@@ -11,9 +11,12 @@
 
 */
 
+//? 슬라이드 전체 내용 및 버튼 -----------
+
 var jsonData = $.getJSON('../json/osam_site_main_slide_01_data.json');
 jsonData.done(function(data){
-  //* 변수
+
+  //* 변수 -----------------------------
   var slideData = data;
   var slideType = 'position_slide';
   var dataLen = slideData.length
@@ -21,11 +24,11 @@ jsonData.done(function(data){
   var SetNum = 0;
 
 
-  //* 함수
-  var slideBtn = function(){
+  //* 함수 -------------------------------
+  var slideBtnMakeFn = function(){
     var insertBtn = '<button type="button" class="next_btn"><span class="blind">다음</span></button><button type="button" class="prev_btn"><span class="blind">이전</span></button>';
     slideWrapperCode.prepend(insertBtn)
-  }
+  } // slideBtnMakeFn : 슬라이드 버튼 html 생성
 
   var slideDivSetfn = function(n){
     slideWrapperCode.append(slideDivSet);
@@ -54,15 +57,21 @@ jsonData.done(function(data){
     imgCaption.text(slideN.image_description);
     imgContent.text(slideN.summary);
 
-  }; // slideDivSetfn
+  }; // slideDivSetfn : 슬라이드 내용에 css 적용 및 text 삽입
 
   var actionFn = function(i){
+    
+    console.log(i-1, i)
     viewCover = $('.view_cover');
     viewCover.eq(i).addClass('action')
-  };  // actionFn
+
+    viewCover.eq(i).fadeIn()
+    viewCover.eq(i-1).fadeOut()
+
+  };  // actionFn : 다음, 이전 버튼 클릭 시 내용 전환 처리
 
 
-  //* 기능구현 1
+  //* 사전 기능 실행 ------------------------------------------
   var slideWrapperSet = '<div class="slide"><div class="slide_wrapper"></div></div>';
   viewBox.html(slideWrapperSet);
 
@@ -75,27 +84,20 @@ jsonData.done(function(data){
     slideDivSetfn(i)
   };
 
-  actionFn(SetNum);
-  slideBtn();
+  actionFn(0);
+  slideBtnMakeFn();
 
 
 
-  //* 인디케이터
+  //? 인디케이터 ----------------------------------------
 
-  // 설명
-  /*
-  - 광고개수를 파악하여 인디케이터를 생성
-  - 해당하는 순서에 맞는 인디케이터에 action을 설정하여 인지할 수 있도록
-  */
-
-
-  // 담을 코드 작성
+  //* 사전 기능 실행 및 실행 위한 변수 설정 --------------
   var indiWrapper = '<div class="slide_check_part"><ul class="slide_indicator blind_area"></ul><p><em class="now_view"></em> / <span class="total_view"></span></p></div>';
   var indiCode = '<li><a href="#" data-href="#"><span></span></a></li>';
-
-
-  // 기능설정1 + 변수
   slideWrapperCode.before(indiWrapper)
+
+
+  //* 변수 -----------------------------------
   var SlideCheckPart = viewBox.find('.slide_check_part')
   var indiWrapperSelector = viewBox.find('.slide_indicator');
   var viewLenCkNow = SlideCheckPart.find('.now_view');
@@ -103,7 +105,7 @@ jsonData.done(function(data){
   var indiSelector
   
 
-  // 함수
+  //* 함수 -------------------------------
   var indicatorSetFn = function(n){
     indiWrapperSelector.append(indiCode);
 
@@ -113,33 +115,81 @@ jsonData.done(function(data){
 
     indiLiSpan.text(slideData[n].summary);
     indiLiLink.attr({'data-href':'.'+slideData[n].image_description})
-  }; // indicatorSetFn
+  }; // indicatorSetFn : 인디케이터에 데이터 삽입
 
   var indicatorCheckFn = function(n){
-
     viewLenCkNow.text(n+1);
     viewLenCkTotal.text(dataLen);
-  }; // indicatorCheckFn 
+  }; // indicatorCheckFn : 인디케이터 인덱스 부분 처리
 
 
-  // indicator 생성
+  //* 반복문으로 indicator 생성 --------------------
   var j = 0
   for(  ; j < dataLen ; j += 1 ) {
     indicatorSetFn(j);
   };
 
 
+  //* 인디케이터 및 인디케이터 인덱스에 초기값 설정 ------ 
   indicatorCheckFn(SetNum);
   indiSelector.eq(SetNum).addClass('action');
 
 
-  //* 슬라이드 광고, indicator, 체크번호 모두 동시에 처리되어야 하는 기능
+
+  //? 실제 광고영역 동작처리 ----------------------------
+  //* 구현할 기능 ----------------------
+  /* 
+  - 다음, 이전 버튼을 누르면 광고가 움직이도록 하기
+  - 인디케이터를 누르면 광고가 움직이도록 하기
+  - 마우스를 광고 위에 올리면 일시정지, 마우스가 광고 위에서 벗어나면 일정시간마다 광고 내용 변경
+  */
+
+
+  //* 변수 ------------------------------
+  var nextBtn = viewBox.find('.next_btn')
+  var prevBtn = viewBox.find('.prev_btn')
+
+ 
+  //* 함수 -------------------------------
+  var indiSetFn = function(n){
+    indiSelector.eq(n).addClass('action');
+    indiSelector.eq(n).siblings().removeClass('action');
+  }; // indiSetFn : 선택된 인디케이터에 action 부여하고 나머지 인디케이터에서는 action을 삭제
+
   var actionNumSetFn = function(n){
+    if(n >= dataLen){
+      n = 0;
+      SetNum = n;
+    } else if (n < 0){
+      n = dataLen-1
+      SetNum = n;
+    } 
     actionFn(n);
     indicatorCheckFn(n);
-    indiSelector.eq(n).addClass('action');
-  } // actionNumSetFn
-  
+    indiSetFn(n)
+  }; // actionNumSetFn : actionFn(버튼 클릭 시 내용 전환 및 슬라이드 전환 효과 처리), indicatorCheckFn(인디케이터 인덱스 처리), indiSetFn(인디케이터 처리)가 하나의 값을 공유하여 동시에 동작하도록 처리
+
+
+  //* 이벤트 --------------------------------
+  nextBtn.on('click', function(e){
+    e.preventDefault();
+    SetNum+=1
+    actionNumSetFn(SetNum);
+  });
+
+  prevBtn.on('click', function(e){
+    e.preventDefault();
+    SetNum-=1
+    actionNumSetFn(SetNum);
+  });
+
+  indiSelector.find('a').on('click', function(e){
+    e.preventDefault();
+    var n = $(this).parent().index();
+    SetNum = n;
+    actionNumSetFn(SetNum)
+  });
+
 
 });
 
